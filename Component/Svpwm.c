619d7319861d788f9be949731c7d4f01cdc9e7c9
xjy_Three_phase_Inverter <xhses@outlook.com>
2026-07-29 15:37:41 +0800

    增加三次谐波注入

--- Component/Svpwm.c
+++ Component/Svpwm.c
@@ -5,6 +5,7 @@
 
 #define SVPWM_DUTY_MIN 0.05f
 #define SVPWM_DUTY_MAX 0.95f
+#define SVPWM_VECTOR_MAGNITUDE_SQUARED_EPSILON 1.0e-12f
 
 static float Svpwm_Clamp(float value, float minimum, float maximum)
 {
@@ -23,8 +24,12 @@ void Svpwm_Calculate(float ud,
     float ua;
     float ub;
     float uc;
+#if SVPWM_MODULATION_MODE == SVPWM_MODE_MIN_MAX_ZERO_SEQUENCE
     float u_max;
     float u_min;
+#else
+    float magnitude_squared;
+#endif
     float u_zero;
 
     if (duty == NULL)
@@ -47,9 +52,23 @@ void Svpwm_Calculate(float ud,
     ub = -0.5f * u_alpha + 0.8660254f * u_beta;
     uc = -0.5f * u_alpha - 0.8660254f * u_beta;
 
+#if SVPWM_MODULATION_MODE == SVPWM_MODE_MIN_MAX_ZERO_SEQUENCE
     u_max = fmaxf(ua, fmaxf(ub, uc));
     u_min = fminf(ua, fminf(ub, uc));
     u_zero = -0.5f * (u_max + u_min);
+#else
+    magnitude_squared = u_alpha * u_alpha + u_beta * u_beta;
+    if (magnitude_squared > SVPWM_VECTOR_MAGNITUDE_SQUARED_EPSILON)
+    {
+        u_zero = -(u_alpha * (u_alpha * u_alpha
+                              - 3.0f * u_beta * u_beta))
+                 / (6.0f * magnitude_squared);
+    }
+    else
+    {
+        u_zero = 0.0f;
+    }
+#endif
 
     duty->duty_a = Svpwm_Clamp(0.5f + (ua + u_zero) / vdc,
                                SVPWM_DUTY_MIN,
